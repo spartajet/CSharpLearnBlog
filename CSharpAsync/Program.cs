35d05d8b29df8399f6f58490eef7d09a58637aac
spartajet <spartajet.guo@gmail.com>
2018-08-25 19:03:13 +0800

    线程基础用法

--- CSharpAsync/Program.cs
+++ CSharpAsync/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
-
+using System.Threading;
 namespace CSharpAsync
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var workThread=new Thread(NumberCountCoudDelay);
+            Console.WriteLine($"work thread state: {workThread.ThreadState}");
+            workThread.Abort();
+            workThread.Start();
+            workThread.Join();
+            Console.WriteLine($"work thread state: {workThread.ThreadState}");
+            Console.WriteLine("Work thread is stopped!!!");
+        }
+
+        /// <summary>
+        /// 数数方法
+        /// </summary>
+        static void NumberCountCoudDelay(){
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Delay thread number is {i}");
+                Console.WriteLine($"work thread state: {Thread.CurrentThread.ThreadState}");
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+        }
+        
+        /// <summary>
+        /// 数数方法
+        /// </summary>
+        static void NumberCount(){
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Main thread number is {i}");
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
         }
     }
 }
